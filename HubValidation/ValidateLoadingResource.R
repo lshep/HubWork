20e28ec3d5496294d6a0b4fd93fe504a8cb19d61
lshep <lori.shepherd@roswellpark.org>
2025-07-18 14:50:13 +0000

    Add some debugging and references to clean up script

--- HubValidation/ValidateLoadingResource.R
+++ HubValidation/ValidateLoadingResource.R
@@ -1,12 +1,17 @@
 ## Ensure main classes of Bioconductor have been installed before running
 ## There will be some that are not installed that are required
 ## Ideal pre-install any package associated with and required packages
+##  to speed up change to install unique(package(eh)) before running
+## I think a few also reference Seurat 
+## there may be more require but not specified -- debug as necessary
+
 
 library(AnnotationHub)
 library(ExperimentHub)
 library(BiocFileCache)
 library(httr)
 library(BiocManager)
+library(dplyr)
 
 ##
 ## Start only with those we say should be currently available
@@ -14,13 +19,19 @@ library(BiocManager)
 
 
 eh <- ExperimentHub()
-EHcheck = rep(TRUE, length(eh))
-EHpackage = rep(TRUE, length(eh))
+#EHcheck = rep(TRUE, length(eh))
+#EHpackage = rep(TRUE, length(eh))
+load("EH_PartialSave_LoadingValidation.RData")
 
 pkgsList = BiocManager::available()
+skipped = c(3272,6970,6971,6972,7296)
 
-for(i in 1:length(eh)){
+## 6970 maxed CPU and RAM 
+
+for(i in 6973:length(eh)){
     message("EH: ", i, " of ", length(eh))
+
+if (!(i %in% skipped)){
     id = rownames(mcols(eh[i]))
     res_pkg = as.character(package(eh[i]))
     if (!(res_pkg %in% installed.packages()[,"Package"])){
@@ -40,6 +51,9 @@ for(i in 1:length(eh)){
     }, error=function(err){FALSE})
     removeResources(eh, ids=id)
     EHpackage[i] = package(eh[i])
+}else{
+	save.image("EH_PartialSave_LoadingValidation.RData")
+}
 }
 EHids = rownames(mcols(eh))
 
@@ -47,6 +61,13 @@ EHids = rownames(mcols(eh))
 save(EHids, EHcheck, EHpackage, file="EH_LoadingValidation.RData")
 
 
+
+##
+## Before running this consult resource size evaluation
+## Also investigate recipes/package requirements  
+##
+
+if(FALSE){
 ah <- AnnotationHub()
 AHcheck = rep(TRUE, length(ah))
 AHpreparerclass = rep(TRUE, length(ah))
@@ -64,3 +85,4 @@ for(i in 1:length(ah)){
 AHids = rownames(mcols(ah))
 
 save(AHids, AHcheck, AHpreparerclass, file="AH_LoadingValidation.RData")
+}
